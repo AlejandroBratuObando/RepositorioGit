b295920c63637ca6b3da6e05750d63ce8f2ae563
AlejandroBratuObando <alexbratu9395@gmail.com>
2025-03-31 01:32:58 +0200

    RamaMaster.java

--- ControlDeVersiones/src/Paquete1/RamaMaster.java
+++ ControlDeVersiones/src/Paquete1/RamaMaster.java
@@ -5,7 +5,11 @@ public class RamaMaster {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hola Master");
+		Scanner sc= new Scanner(System.in);
+		
+		System.out.println("Dime una palabra");
+		String palabra= sc.next();
+		System.out.println("Esta es tu palabra: " + palabra);
 
 	}
 
